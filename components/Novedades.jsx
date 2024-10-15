760ec849c6cf689419362a58ac213467772004ab
Steven Trujillo <139834763+imTrujillo@users.noreply.github.com>
2024-10-14 19:36:05 -0600

    actualizar imgs

--- components/Novedades.jsx
+++ components/Novedades.jsx
@@ -1,120 +1,144 @@
-import React from 'react'
-import styles from '../styles/novedades.module.css'
+import React from 'react';
+import styles from '../styles/novedades.module.css';
 import { MdArrowForwardIos } from "react-icons/md";
 import { BsThreeDots } from "react-icons/bs";
 
+import n1 from '../assets/n1.png';
+import n2 from '../assets/n2.png';
+import n3 from '../assets/n3.png';
+import n4 from '../assets/n4.png';
+import nc1 from '../assets/nc1.png';
+import nc2 from '../assets/nc2.png';
+import nc3 from '../assets/nc3.png';
+import nc4 from '../assets/nc4.png';
+import nc5 from '../assets/nc5.png';
+import nc6 from '../assets/nc6.png';
+import nc7 from '../assets/nc7.png';
+import nc8 from '../assets/nc8.png';
 
 export default function Novedades() {
   return (
     <div className={styles.container}>
-    <h1 className={styles.border}>Novedades</h1>
-    
-    <section className={styles.cardContainer}>
+      <h1 className={styles.border}>Novedades</h1>
+      
+      <section className={styles.cardContainer}>
         <div className={styles.card}>
-            <p>NUEVO ÁLBUM</p>
-            <h6>LA CASA DE LA PLAYA</h6>
-            <span>Miami</span>
-            <img src="../src/assets/n1.png" alt="" />
+          <p>NUEVO ÁLBUM</p>
+          <h6>LA CASA DE LA PLAYA</h6>
+          <span>Miami</span>
+          <img src={n1} alt="La Casa de la Playa" />
         </div>
         <div className={styles.card}>
-            <p>NUEVO ÁLBUM</p>
-            <h6>Ejército de salvación</h6>
-            <span>Love of Lesbian</span>
-            <img src="../src/assets/n2.png" alt="" />
+          <p>NUEVO ÁLBUM</p>
+          <h6>Ejército de salvación</h6>
+          <span>Love of Lesbian</span>
+          <img src={n2} alt="Ejército de salvación" />
         </div>
         <div className={styles.card}>
-            <p>NUEVO ÁLBUM</p>
-            <h6>SORA</h6>
-            <span>Ganges</span>
-            <img src="../src/assets/n3.png" alt="" />
+          <p>NUEVO ÁLBUM</p>
+          <h6>SORA</h6>
+          <span>Ganges</span>
+          <img src={n3} alt="SORA" />
         </div>
         <div className={styles.card}>
-            <span>UP NEXT</span>
-            <h6>Mafalda Cardenal es nueva artista Up Next de octubre</h6>
-            <img src="../src/assets/n4.png" alt="" />
+          <span>UP NEXT</span>
+          <h6>Mafalda Cardenal es nueva artista Up Next de octubre</h6>
+          <img src={n4} alt="Mafalda Cardenal Up Next" />
         </div>
-    </section>
-
-    <h2 className={styles.border}>Canciones recién llegadas        <MdArrowForwardIos /></h2>
-    <section className={styles.cardContainer2}>
+      </section>
+  
+      <h2 className={styles.border}>
+        Canciones recién llegadas <MdArrowForwardIos />
+      </h2>
+      <section className={styles.cardContainer2}>
         <div className={styles.column}>
-        <div className={styles.card2}>
-        <div className={styles.card2Inner}>
-            <img src="../src/assets/nc1.png" alt="" />
-            <div>
-            <p>Sympathy is a knife</p>
-            <span>Charli xcx - Ariana Grande</span>
-            </div>
-        </div>
-            <BsThreeDots />
-        </div>
-        <div className={styles.card2}>
+          <div className={styles.card2}>
             <div className={styles.card2Inner}>
-            <img src="../src/assets/nc2.png" alt="" />
-            <div><p>La Champions y el Mundo</p>
-            <span>Love of Lesbian</span>
-            </div>
+              <img src={nc1} alt="Sympathy is a knife" />
+              <div>
+                <p>Sympathy is a knife</p>
+                <span>Charli xcx - Ariana Grande</span>
+              </div>
             </div>
             <BsThreeDots />
-        </div>
-        <div className={styles.card2}>
+          </div>
+          <div className={styles.card2}>
             <div className={styles.card2Inner}>
-            <img src="../src/assets/nc3.png" alt="" />
-            <div><p>Cómo Quieres Que Te Quiera</p>
-            <span>Rosario</span></div>
+              <img src={nc2} alt="La Champions y el Mundo" />
+              <div>
+                <p>La Champions y el Mundo</p>
+                <span>Love of Lesbian</span>
+              </div>
             </div>
             <BsThreeDots />
-        </div>
-        <div className={styles.card2}>
+          </div>
+          <div className={styles.card2}>
             <div className={styles.card2Inner}>
-            <img src="../src/assets/nc4.png" alt="" />
-            <div><p>Contigo</p>
-            <span>Malú - Prince Royce</span></div>
+              <img src={nc3} alt="Cómo Quieres Que Te Quiera" />
+              <div>
+                <p>Cómo Quieres Que Te Quiera</p>
+                <span>Rosario</span>
+              </div>
             </div>
             <BsThreeDots />
-        </div>
+          </div>
+          <div className={styles.card2}>
+            <div className={styles.card2Inner}>
+              <img src={nc4} alt="Contigo" />
+              <div>
+                <p>Contigo</p>
+                <span>Malú - Prince Royce</span>
+              </div>
+            </div>
+            <BsThreeDots />
+          </div>
         </div>
         <div className={styles.column}>
-        <div className={styles.card2}>
+          <div className={styles.card2}>
             <div className={styles.card2Inner}>
-            <img src="../src/assets/nc5.png" alt="" />
-            <div><p>Vuela</p>
-            <span>Nena Daconte</span></div>
+              <img src={nc5} alt="Vuela" />
+              <div>
+                <p>Vuela</p>
+                <span>Nena Daconte</span>
+              </div>
             </div>
             <BsThreeDots />
-        </div>
-        <div className={styles.card2}>
+          </div>
+          <div className={styles.card2}>
             <div className={styles.card2Inner}>
-            <img src="../src/assets/nc6.png" alt="" />
-            <div><p>Que nadie me llore</p>
-            <span>Funambulista - La La Love</span></div>
+              <img src={nc6} alt="Que nadie me llore" />
+              <div>
+                <p>Que nadie me llore</p>
+                <span>Funambulista - La La Love</span>
+              </div>
             </div>
             <BsThreeDots />
-        </div>
-        <div className={styles.card2}>
+          </div>
+          <div className={styles.card2}>
             <div className={styles.card2Inner}>
-            <img src="../src/assets/nc7.png" alt="" />
-            <div><p>Besitos BB 2.0</p>
-            <span>La Perla - Omar Montes</span></div>
+              <img src={nc7} alt="Besitos BB 2.0" />
+              <div>
+                <p>Besitos BB 2.0</p>
+                <span>La Perla - Omar Montes</span>
+              </div>
             </div>
             <BsThreeDots />
-            
-        </div>
-        <div className={styles.card2}>
+          </div>
+          <div className={styles.card2}>
             <div className={styles.card2Inner}>
-            <img src="../src/assets/nc8.png" alt="" />
-            <div><p>No me duele</p>
-            <span>Morad</span></div>
+              <img src={nc8} alt="No me duele" />
+              <div>
+                <p>No me duele</p>
+                <span>Morad</span>
+              </div>
             </div>
             <BsThreeDots />
-            
+          </div>
         </div>
-        </div>
-        
-    </section>
-    
-    <h2 className={styles.border}>Explora más</h2>
-    <section className={styles.btnContainer}>
+      </section>
+      
+      <h2 className={styles.border}>Explora más</h2>
+      <section className={styles.btnContainer}>
         <button>Explorar por género<MdArrowForwardIos /></button>
         <button>Décadas<MdArrowForwardIos /></button>
         <button>Actividades y estados de ánimo<MdArrowForwardIos /></button>
@@ -122,7 +146,7 @@ export default function Novedades() {
         <button>Listas<MdArrowForwardIos /></button>
         <button>Audio espacial<MdArrowForwardIos /></button>
         <button>Videoclips<MdArrowForwardIos /></button>
-    </section>
+      </section>
     </div>
   )
 }
--- components/Radio.jsx
+++ components/Radio.jsx
@@ -1,101 +1,107 @@
-import React from 'react'
-import styles from '../styles/novedades.module.css'
+import React from 'react';
+import styles from '../styles/novedades.module.css';
 import { BsThreeDots } from "react-icons/bs";
 
+import r1 from '../assets/r1.png';
+import r2 from '../assets/r2.png';
+import r3 from '../assets/r3.png';
+import re1 from '../assets/re1.png';
+import re2 from '../assets/re2.png';
+import re3 from '../assets/re3.png';
+import re4 from '../assets/re4.png';
 
 export default function Radio() {
   return (
     <div className={styles.container}>
-    <h1 className={styles.border}>Radio</h1>
-        
-    <section className={styles.cardContainer}>
+      <h1 className={styles.border}>Radio</h1>
+      
+      <section className={styles.cardContainer}>
         <div className={styles.card}>
-            <h3>Music 1</h3>
-            <p>Música nueva y relevante.</p>
-            <div>
-            <img src="../src/assets/r1.png" alt="" />
+          <h3>Music 1</h3>
+          <p>Música nueva y relevante.</p>
+          <div>
+            <img src={r1} alt="Music 1" />
             <div className={styles.infoCard}>
-            <p>EN DIRECTO - 16:00-18:00</p>
-            <span>Time Crisis</span>
-            <p>The Crew talk Red Lobster and shellflish-themed...</p>
+              <p>EN DIRECTO - 16:00-18:00</p>
+              <span>Time Crisis</span>
+              <p>The Crew talk Red Lobster and shellfish-themed...</p>
             </div>
-            </div>
+          </div>
         </div>
         <div className={styles.card}>
-        <h3>Music Hits</h3>
-            <p>Música nueva y relevante.</p>
-            <div><img src="../src/assets/r2.png" alt="" />
+          <h3>Music Hits</h3>
+          <p>Música nueva y relevante.</p>
+          <div>
+            <img src={r2} alt="Music Hits" />
             <div className={styles.infoCard}>
-            <p>EN DIRECTO - 17:00-19:00</p>
-            <span>The Hits List</span>
-            <p>Songs you know and love.</p>
+              <p>EN DIRECTO - 17:00-19:00</p>
+              <span>The Hits List</span>
+              <p>Songs you know and love.</p>
             </div>
-            </div>
+          </div>
         </div>
         <div className={styles.card}>
-        <h3>Music Country</h3>
-            <p>La casa del country.</p>
-            <div>
-            <img src="../src/assets/r3.png" alt="" />
+          <h3>Music Country</h3>
+          <p>La casa del country.</p>
+          <div>
+            <img src={r3} alt="Music Country" />
             <div className={styles.infoCard}>
-            <p>EN DIRECTO - 18:00-19:00</p>
-            <span>Country Rewind</span>
-            <p>Timeless country hits you can revisit again and again.</p>
+              <p>EN DIRECTO - 18:00-19:00</p>
+              <span>Country Rewind</span>
+              <p>Timeless country hits you can revisit again and again.</p>
             </div>
-            </div>
+          </div>
         </div>
-    </section>
-    
-        <h2 className={styles.border}>Emisores locales</h2>
-    <section className={styles.cardContainer2}>
+      </section>
+      
+      <h2 className={styles.border}>Emisores locales</h2>
+      <section className={styles.cardContainer2}>
         <div className={styles.column}>
-        <div className={styles.card2}>
+          <div className={styles.card2}>
             <div className={styles.card2Inner}>
-            <img src="../src/assets/re1.png" alt="" />
-            <div>
-            <p>Cadena 100</p>
-            <p>La mejor variedad musical.</p>
-            </div>
+              <img src={re1} alt="Cadena 100" />
+              <div>
+                <p>Cadena 100</p>
+                <p>La mejor variedad musical.</p>
+              </div>
             </div>
             <BsThreeDots />
-        </div>
-        <div className={styles.card2}>
+          </div>
+          <div className={styles.card2}>
             <div className={styles.card2Inner}>
-            <img src="../src/assets/re2.png" alt="" />
-            <div>
-            <p>KISS FM (ESPAÑA)</p>
-            <p>TuneIn</p>
+              <img src={re2} alt="KISS FM (ESPAÑA)" />
+              <div>
+                <p>KISS FM (ESPAÑA)</p>
+                <p>TuneIn</p>
+              </div>
             </div>
-            </div>
-            
             <BsThreeDots />
-        </div>
+          </div>
         </div>
         
         <div className={styles.column}>
-        <div className={styles.card2}>
+          <div className={styles.card2}>
             <div className={styles.card2Inner}>
-            <img src="../src/assets/re3.png" alt="" />
-            <div><p>LOS40</p>
-            <p>TuneIn</p></div>
+              <img src={re3} alt="LOS40" />
+              <div>
+                <p>LOS40</p>
+                <p>TuneIn</p>
+              </div>
             </div>
             <BsThreeDots />
-        </div>
-        <div className={styles.card2}>
+          </div>
+          <div className={styles.card2}>
             <div className={styles.card2Inner}>
-            <img src="../src/assets/re4.png" alt="" />
-            <div>
-            <p>Rock FM</p>
-            <p>La radio viva del rock.</p>
+              <img src={re4} alt="Rock FM" />
+              <div>
+                <p>Rock FM</p>
+                <p>La radio viva del rock.</p>
+              </div>
             </div>
-            </div>
-            
             <BsThreeDots />
+          </div>
         </div>
-        </div>
-        
-        
-    </section>
+      </section>
     </div>
   )
 }
